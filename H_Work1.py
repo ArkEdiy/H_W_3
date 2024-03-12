1f62777941ffad44116244349024d7f2670102c8
arkediy <artur32827@gmail.com>
2024-03-12 17:44:17 +0200

    re

--- H_Work1.py
+++ H_Work1.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    transf_date = datetime.strptime(date, '%Y-%m-%d')
-    current_date = datetime.today()
-    difference_days = (transf_date - current_date).days
-    return difference_days
+    try:
+        transf_date = datetime.strptime(date, '%Y-%m-%d')
+        current_date = datetime.today()
+        difference_days = (transf_date - current_date).days
+        return difference_days
+    except ValueError:
+        print("Неправильний формат дати.Використайте формат 'рік-місяць-день'.")
+        return None
 
 date_task = '2020-10-09'
 result = get_days_from_today(date_task)
