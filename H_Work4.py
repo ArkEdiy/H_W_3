6511c6b76fc8ddda67917a7dbe0949a44b75772f
arkediy <artur32827@gmail.com>
2024-03-14 00:03:39 +0200

    end

--- H_Work4.py
+++ H_Work4.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timedelta
 
 users = [
-    {"name": "John Doe", "birthday": "1985.01.23"},
-    {"name": "Jane Smith", "birthday": "1990.01.27"},
-    {"name": "Adelina Ford", "birthday": "1999.03.13"},
+    {"name": "John Doe", "birthday": "1985.03.2"},
+    {"name": "John Doe", "birthday": "1985.03.7"},
+    {"name": "John Doe", "birthday": "1985.03.10"},
+    {"name": "Jane Smith", "birthday": "1990.03.11"},
+    {"name": "Adelina Ford", "birthday": "1999.03.14"},
     {"name": "Adelan Hord", "birthday": "1997.03.16"}
 ]
 
@@ -15,17 +17,16 @@ def get_upcoming_birthdays(users):
         birthday = datetime.strptime(user["birthday"], "%Y.%m.%d").date()
         birthday_this_year = datetime(today.year, birthday.month, birthday.day).date() # визначення дн для поточного року
 
-        # якщо дн вже було в цьому році, переносимо його на наступний рік
-        if birthday_this_year < today:
-            birthday_this_year += timedelta(days=365)
+        start_of_week = today - timedelta(days=today.weekday()) #Визначення першого дня поточного тижня (понеділок)
 
-        days_until_birthday = (birthday_this_year - today).days # знаходження кількості днів до дн
+        if start_of_week <= birthday_this_year <= start_of_week + timedelta(days=7):
+            # якщо дн вже було в цьому році, переносимо його на наступний рік
+            if birthday_this_year < today:
+                birthday_this_year += timedelta(days=365)
 
-        # Перевірка,чи день народженя випадає вперед на 7 днів
-        if 0 <= days_until_birthday <= 7:
-           while birthday_this_year.weekday() >= 5: #Перенесення дати привітання на наступний понеділок, якщо вихідний
-            birthday_this_year += timedelta(days=1)
-            
+            while birthday_this_year.weekday() >= 5: #Перенесення дати привітання на наступний понеділок, якщо вихідний
+             birthday_this_year += timedelta(days=1)
+       
             #додавання інформації до списку
             upcoming_birthdays.append({
                 "name": user["name"],
